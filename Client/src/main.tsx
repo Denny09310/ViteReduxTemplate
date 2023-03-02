de3f51a93e1b35370deb29a92d8ae8b298c119f1
Koja sig. Dennis <DKOJA@ceia-spa.com>
2023-03-02 08:40:37 +0100

    - Moved aspnetcore files outside src - Renamed index.css in main.css - Bumped up dependencies

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,18 +1,19 @@
-import App from "@/App";
-import store from "@/app/store";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import App from '@/App';
+import store from '@/app/store';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import "@/index.css";
-import "@/sw.registration";
+import '@/main.css';
+import '@/sw.registration';
 
-const base = document.head.getElementsByTagName("base")[0].getAttribute("href");
+const base = document.head.getElementsByTagName('base')[0].getAttribute('href');
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Provider store={store}>
-    <Router basename={base!}>
-      <App />
-    </Router>
-  </Provider>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<Provider store={store}>
+		<Router basename={base!}>
+			<App />
+		</Router>
+	</Provider>
 );
+
